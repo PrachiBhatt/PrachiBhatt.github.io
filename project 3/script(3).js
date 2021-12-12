8b4e0d0cb5c767a80d7dcecdde3ccb089ea9d5cc
Prachi Bhatt <prachibhatt@Prachis-MacBook-Air.local>
2021-12-12 12:20:01 +0530

    calculator and countDownTimer added

---
+++ project 3/script(3).js
@@ -0,0 +1,7 @@
+https://www.jaykayonline.com/slider/slider4.jpg
+
+https://www.jaykayonline.com/slider/slider5.jpg
+
+https://www.jaykayonline.com/slider/slider3.jpg
+
+https://www.jaykayonline.com/slider/slider6.jpg
